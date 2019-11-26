d3a6ac3ab6d7cf0caed57c58559bf0ec307089d5
Loredana <loredana.cirstea@gmail.com>
2019-11-26 23:23:18 +0200

    Add more Rust functions

--- nodejs/native/src/lib.rs
+++ nodejs/native/src/lib.rs
@@ -34,4 +34,30 @@ export! {
     fn sum(n1: i32, n2: i32) -> i32 {
         n1 + n2
     }
+
+    fn concat(n1: Vec<i32>, n2: Vec<i32>) -> Vec<i32> {
+        let mut a = n1.to_vec();
+        a.extend(n2);
+        a
+    }
+
+    fn sort(n1: Vec<i32>) -> Vec<i32> {
+        let mut a = n1.to_vec();
+        a.sort();
+        a
+    }
+
+    fn slice(n1: Vec<i32>, start: i32, end: i32) -> Vec<i32> {
+        let ustart = start as usize;
+        let uend = end as usize;
+        n1[ustart..uend].to_vec()
+    }
+
+    fn length(n1: Vec<i32>) -> i32 {
+        n1.len() as i32
+    }
+
+    fn executionEnvVersion() -> String {
+        format!("rustc version: {}", version())
+    }
 }
